f4e63a87496b7cdd4c77a457c1b07785bfb75727
Ignacio Femenia <femeniax@gmail.com>
2022-07-09 12:38:33 -0300

    CuentaAhorro.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
+import { CuentaAhorro } from './CuentaAhorro.js';
 import {CuentaCorriente} from './CuentaCorriente.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
@@ -9,6 +10,11 @@ const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
 const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
 
 let saldo = cuentaDeLeonardo.verSaldo(); 
+
+const CuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0);
+console.log(CuentaAhorroLeonardo)
+
+
 
 saldo = cuentaDeLeonardo.depositoEnCuenta(150);
 cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
@@ -24,7 +30,6 @@ console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
 console.log(CuentaCorriente.cantidadCuentas);
 
 console.log(CuentaCorriente.cantidadCuentas);
-
 
 
 
